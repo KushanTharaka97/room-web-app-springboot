af31244ed2d2fcaa8a9ed3c6ea646a5e85cf0950
Kushan Tharaka <tharakakushan@gmail.com>
2020-06-25 14:55:24 +0530

    Initial Commit

---
+++ src/test/java/com/kushan/KT2020/roomwebapp/RoomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushan.KT2020.roomwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
